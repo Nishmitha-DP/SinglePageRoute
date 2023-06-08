7dc36e9c67ddb8c4a503972c4a32b49f92c7241b
Nishmitha-DP <131871153+Nishmitha-DP@users.noreply.github.com>
2023-06-08 17:30:34 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React,{useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import ColorsPage from './components/ColorsPage';
-import CitiesPage from './components/CitiesPage';
-import Links from './components/Links';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Links from "./components/Links";
+import ColorsPage from "./components/ColorsPage";
+import CitiesPage from "./components/CitiesPage";
 
 function App() {
-
   const [selectedCity, setSelectedCity] = useState('');
   const [selectedPlaces, setSelectedPlaces] = useState([]);
   const [selectedColor, setSelectedColor] = useState('');
 
+  useEffect(() => {
+    const storedSelectedCity = localStorage.getItem('selectedCity');
+    const storedSelectedPlaces = localStorage.getItem('selectedPlaces');
+    const storedSelectedColor = localStorage.getItem('selectedColor');
+
+    if (storedSelectedCity) setSelectedCity(storedSelectedCity);
+    if (storedSelectedPlaces) setSelectedPlaces(storedSelectedPlaces);
+    if (storedSelectedColor) setSelectedColor(storedSelectedColor);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('selectedCity', selectedCity);
+    localStorage.setItem('selectedPlaces', selectedPlaces);
+    localStorage.setItem('selectedColor', selectedColor);
+  }, [selectedCity, selectedPlaces, selectedColor]);
+
   return (
     <div className="App">
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Links/>}/>
-      <Route path='/colorspage' element={<ColorsPage selectedColor={selectedColor} setSelectedColor={setSelectedColor}/>}/>
-      <Route path='/citiespage' element={<CitiesPage selectedCity={selectedCity} setSelectedCity={setSelectedCity} selectedPlaces={selectedPlaces} setSelectedPlaces={setSelectedPlaces}/>}/>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Links />} />
+          <Route path='/colorspage' element={<ColorsPage selectedColor={selectedColor} setSelectedColor={setSelectedColor} />} />
+          <Route path='/citiespage' element={<CitiesPage selectedCity={selectedCity} setSelectedCity={setSelectedCity} selectedPlaces={selectedPlaces} setSelectedPlaces={setSelectedPlaces} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
